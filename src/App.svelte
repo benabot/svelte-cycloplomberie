4909b8c606859f228e8207e03303e62eae73134e
benabot <benoitabot@yahoo.fr>
2023-03-06 21:37:48 +0100

    precisions fuites

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,8 @@
   };
   const decrement = () => {
     count -= 1;
+
+    etape[count] = "";
   };
 
   let etape1;
@@ -37,7 +39,7 @@
       id: "22-1",
       name1: "Au sol",
       src: "/pictos/chaudiere-sol.svg",
-      prix: "90",
+      prix: "120",
     },
     {
       id: "22-2",
@@ -47,6 +49,16 @@
     },
   ];
   let level31 = [
+    {
+      id: "31-10",
+      name1: "Arrivée d'eau",
+      src: "/pictos/arrivee-eau.svg",
+    },
+    {
+      id: "31-11",
+      name1: "Canalisations",
+      src: "/pictos/canalisation.svg",
+    },
     { id: "31-1", name1: "Évier", src: "/pictos/evier.svg" },
     {
       id: "31-2",
@@ -95,11 +107,7 @@
       name1: "Canalisation",
       src: "/pictos/canalisation.svg",
     },
-    {
-      id: "32-11",
-      name1: "Arrivée d'eau",
-      src: "/pictos/arrivee-eau.svg",
-    },
+
     { id: "32-1", name1: "Évier", src: "/pictos/evier.svg" },
     {
       id: "32-2",
@@ -142,6 +150,34 @@
       src: "/pictos/autre.svg",
     },
   ];
+  let fuites = [
+    "En sortie du robinet",
+    "À la base du robinet",
+    "Au niveau des flexibles",
+    "Au niveau du siphon/évacuation",
+    "Au niveau des joints silicone",
+    "Je ne sais pas",
+  ];
+  let fuites2 = [
+    "Au niveau du robinet",
+    "Au niveau du siphon/évacuation",
+    "Au niveau des joints silicone",
+    "Je ne sais pas",
+  ];
+  let fuites3 = [
+    "Près du robinet d'arrêt",
+    "Entre la cuvette et le reservoir",
+    "Sur la pipe d'évacuation",
+    "Dans la cuvette",
+    "Je ne sais pas",
+  ];
+  let fuites4 = ["Au niveau du mécanisme de chasse", "Je ne sais pas"];
+  let fuites5 = ["Au niveau de l'alimentation en eau", "Je ne sais pas"];
+  let fuites6 = [
+    "Au niveau d'une vanne/robinet",
+    "Tuyeau percé, abîmé",
+    "Je ne sais pas",
+  ];
 </script>
 
 <main>
@@ -151,6 +187,7 @@
   <h1>Formulaire</h1>
   <div class="container">
     {#if count === 1}
+      <h2 class="titre-chaudiere">Décrivez votre problème ou votre projet</h2>
       {#each level1 as { id, name1, src }}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div
@@ -219,6 +256,7 @@
     {/if}
 
     {#if count === 3 && etape2 === "Canalisation bouchée"}
+      <h2 class="titre-chaudiere">Le problème concerne :</h2>
       {#each level32 as { id, name1, src }}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div
@@ -236,32 +274,144 @@
     {/if}
 
     {#if goform === true || etape1 === "Pose"}
-      <form action="" class="cpa-form">
-        Vous nous contactez pour : {etape1.toLowerCase()}
-        {#if etape2}
-          {etape2.toLowerCase()}
-        {/if}
-        {#if etape3}
-          {etape3.toLowerCase()}
-        {/if}
-        <h2>Renseignements</h2>
-        <div class="info">
-          <input
-            class="fname"
-            type="text"
-            name="name"
-            placeholder="Full name"
-          />
-          <input type="text" name="name" placeholder="Email" />
-          <input type="text" name="name" placeholder="Phone number" />
-          <input type="text" name="name" placeholder="Website" />
+      <div class="container-form">
+        <p class="probleme-resume">
+          Vous nous contactez pour : <span class="strong"
+            >{etape1.toLowerCase()}
+            {#if etape2}
+              {etape2.toLowerCase()}
+            {/if}
+            {#if etape3}
+              {etape3.toLowerCase()}
+            {/if}
+            {#if etape4}
+              {etape4.toLowerCase()}
+            {/if}
+          </span>
+        </p>
+        <div class="fuite-localisation">
+          <p>
+            Plus précisément où se situe la fuite sur {#if etape3 === "Lavabo" || etape3 === "Lave-linge"}
+              le
+            {:else if etape3 === "Évier" || etape3 === "Arrivée d'eau"}l'
+            {:else if etape3 === "WC suspendues" || etape3 === "Canalisations" || etape3 === "WC classiques"}les
+            {:else}
+              la
+            {/if}
+            {etape3.toLowerCase()} :
+          </p>
+          <div class="fuite-localisation-list">
+            {#if etape3 === "Lavabo" || etape3 === "Évier"}
+              {#each fuites as fuite}
+                <label class="form-control">
+                  <input
+                    class="checkmark"
+                    type="radio"
+                    bind:group={etape4}
+                    name="fuites"
+                    value={fuite}
+                  />
+                  {fuite}
+                </label>
+              {/each}
+            {/if}
+
+            {#if etape3 === "Baignoire" || etape3 === "Douche"}
+              {#each fuites2 as fuite2}
+                <label class="form-control">
+                  <input
+                    class="checkmark"
+                    type="radio"
+                    bind:group={etape4}
+                    name="fuites2"
+                    value={fuite2}
+                  />
+                  {fuite2}
+                </label>
+              {/each}
+            {/if}
+
+            {#if etape3 === "WC classiques"}
+              {#each fuites3 as fuite}
+                <label class="form-control">
+                  <input
+                    class="checkmark"
+                    type="radio"
+                    bind:group={etape4}
+                    name="fuites3"
+                    value={fuite}
+                  />
+                  {fuite}
+                </label>
+              {/each}
+            {/if}
+
+            {#if etape3 === "WC suspendues"}
+              {#each fuites4 as fuite}
+                <label class="form-control">
+                  <input
+                    class="checkmark"
+                    type="radio"
+                    bind:group={etape4}
+                    name="fuites4"
+                    value={fuite}
+                  />
+                  {fuite}
+                </label>
+              {/each}
+            {/if}
+
+            {#if etape3 === "Lave-linge"}
+              {#each fuites5 as fuite}
+                <label class="form-control">
+                  <input
+                    class="checkmark"
+                    type="radio"
+                    bind:group={etape4}
+                    name="fuites5"
+                    value={fuite}
+                  />
+                  {fuite}
+                </label>
+              {/each}
+            {/if}
+
+            {#if etape3 === "Canalisations" || etape3 === "Arrivée d'eau"}
+              {#each fuites6 as fuite}
+                <label class="form-control">
+                  <input
+                    class="checkmark"
+                    type="radio"
+                    bind:group={etape4}
+                    name="fuites6"
+                    value={fuite}
+                  />
+                  {fuite}
+                </label>
+              {/each}
+            {/if}
+          </div>
         </div>
-        <p>Message</p>
-        <div>
-          <textarea rows="4" />
-        </div>
-        <button type="submit" href="/">Submit</button>
-      </form>
+        <form action="" class="cpa-form">
+          <h2>Renseignements</h2>
+          <div class="info">
+            <input
+              class="fname"
+              type="text"
+              name="name"
+              placeholder="Full name"
+            />
+            <input type="text" name="name" placeholder="Email" />
+            <input type="text" name="name" placeholder="Phone number" />
+            <input type="text" name="name" placeholder="Website" />
+          </div>
+          <p>Message</p>
+          <div>
+            <textarea rows="4" />
+          </div>
+          <button type="submit" href="/">Submit</button>
+        </form>
+      </div>
     {/if}
   </div>
   <div>
@@ -279,11 +429,11 @@
   </div>
   <!-- <p>étape {count}</p>
   <ul>
-    <li>{etape1}</li>
-    <li>{etape2}</li>
-    <li>{etape3}</li>
-    <li>{etape4}</li>
-    <li>{goform}</li>
+    <li>1 {etape1}</li>
+    <li>2 {etape2}</li>
+    <li>3 {etape3}</li>
+    <li>4 {etape4}</li>
+    <li>form {goform}</li>
   </ul> -->
 </main>
 
